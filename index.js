c7d9a27b1b9be110429ad52cc5bc7570cb24f1e6
Jose Luis Salazar Gonzalez <joseluis25sg@gmail.com>
2018-06-06 15:35:55 +0200

    Add global replacement

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ String.prototype.format = function() {
     var formatted = this;
 	let replacements = arguments[0]
     for(let key in replacements ) {
-        formatted = formatted.replace("{{" + key + "}}", replacements[key]);
+        formatted = formatted.replace(new RegExp("{{" + key + "}}", "g"), replacements[key]);
     }
     return formatted;
 };
