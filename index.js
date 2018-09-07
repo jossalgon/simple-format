b12d91fc9f5ee5fd57ff687f7fc4fabc27c1e417
Jose Luis Salazar Gonzalez <joseluis25sg@gmail.com>
2018-09-07 10:17:14 +0200

    Support langs

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 String.prototype.format = function() {
-    var formatted = this;
-	let replacements = arguments[0]
-    for(let key in replacements ) {
-        formatted = formatted.replace(new RegExp("{{" + key + "}}", "g"), replacements[key]);
+  var formatted = this;
+  let replacements = arguments[0];
+  let lang = arguments[1];
+  for (let key in replacements) {
+    if (typeof replacements[key] === 'string' || replacements[key] instanceof String) {
+      formatted = formatted.replace(new RegExp("{{" + key + "}}", "g"), replacements[key]);
+    } else {
+      let replacement = (lang && replacements[key][lang]) ? replacements[key][lang] : replacements[key];
+      formatted = formatted.replace(new RegExp('{{' + key + '}}', 'g'), replacement);
     }
-    return formatted;
-};
+  }
+  return formatted;
+}
